0ef0f1dcea5df9ca6a57615472724132b11c2f34
tstyer <travisstyer.ts@gmail.com>
2026-07-26 13:53:26 +0100

    import react hot toaster

--- frontend/Note App/src/main.jsx
+++ frontend/Note App/src/main.jsx
@@ -3,13 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router";
-
+import { Toaster } from 'react-hot-toast';
+ 
 // wrap app in browser router so whole app is in router
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter> 
       <App />
+      <Toaster />
     </BrowserRouter>
     
   </StrictMode>,
