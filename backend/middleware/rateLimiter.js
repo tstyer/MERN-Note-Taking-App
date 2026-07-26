376cfb0274ee770768c689b12ccb590ebb7a7e7d
tstyer <travisstyer.ts@gmail.com>
2026-07-26 12:30:46 +0100

    Create rateLimiter function

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -2,14 +2,20 @@ import { rateLimit } from "../config/upstash.js";
 
 export async function rateLimiter(req,res,next) {
     try {
-        rateLimit();
-    } catch (error) {
+        const {success} = await rateLimit.limit("my-limit-key"); // so i can find where a limit was hit with this key
 
+        if(success) {
+            return res.status(429).json({message: "Too many requests. please try again later."});
+        } else {
+            next() // send the next function - run as expected if limit not reached
+        }
+    } catch (error) {
+        console.log("rate limit error"); // any other errors 
     }
 }
 
 /* Learning
-    # when you write middleware functions, you then call them before you receive API requests, or place them
+
 
 
 */
