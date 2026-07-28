4ec0c7a99d7f298b88e9807a8c6f9941161c15fd
tstyer <travisstyer.ts@gmail.com>
2026-07-28 14:39:10 +0100

    Add new note button

--- frontend/Note App/src/components/NavBar.jsx
+++ frontend/Note App/src/components/NavBar.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const NavBar = () => {
   return (
       <header className='bg-base-300 border-b border-base-content/10'>
         <div className='mx-auto max-w-6xl px-4 py-4'>
           <div className='flex items-center justify-between'> 
+            
             <h1 className='text-3xl font-bold tracking-tight text-neutral font-mono'>Notely</h1>
+
+            <div className='flex items-center gap-4'>
+              <Link to={'/CreatePage'} className='btn btn-neutral'>New Note</Link>
+
+            </div>
+
           </div>
         </div>
 
