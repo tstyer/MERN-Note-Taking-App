b21b7312c28c9106aa3ee1dba0d8152936ec2b66
tstyer <travisstyer.ts@gmail.com>
2026-08-16 14:06:48 +0100

    Begin note card component

---
+++ frontend/Note App/src/components/NoteCard.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'lucide-react'
+import React from 'react'
+
+const NoteCard = () => {
+  return (
+    <Link> {/* need to use a link and not a div because when you link the card it takes to to its own page */}
+      
+    </Link>
+  )
+}
+
+export default NoteCard
--- frontend/Note App/src/pages/HomePage.jsx
+++ frontend/Note App/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import RateLimitedUI from '../components/RateLimitedUI';
 import NavBar from '../components/NavBar';
 import axios from 'axios'
 import toast from 'react-hot-toast';
+import NoteCard from '../components/NoteCard';
 
 const HomePage = () => {
 
@@ -15,7 +16,7 @@ const HomePage = () => {
 
     const fetchNotes = async () => {
     try {  
-      const response = await axios.get("http://localhost:5173/NotePage")
+      const response = await axios.get("http://localhost:5001/api/notes")
       console.log(response.data);
       setNotes(response.data);
       setIsRateLimited(false) // false because if you can get data, it's not rate limited
@@ -45,7 +46,11 @@ const HomePage = () => {
         {loading && <div className='text-primary py-10 text-center'>Loading Notes...</div>}
 
         {notes.length > 0 && !isRateLimited && (
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'></div>
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note} />
+            ))}
+          </div>
         )}
       </div>
 
