6469cf4117368ac80dfd1529ba1b03563f1157d8
tstyer <travisstyer.ts@gmail.com>
2026-08-16 14:22:39 +0100

    Add note header, delete button

--- frontend/Note App/src/components/NoteCard.jsx
+++ frontend/Note App/src/components/NoteCard.jsx
@@ -1,9 +1,36 @@
 import { Link } from 'react-router'
+import { PenSquareIcon, Trash2Icon } from 'lucide-react'
 import React from 'react'
 
 const NoteCard = ({note}) => {
   return (
-    <Link to={`/note/${note._id}`}>NoteCard</Link>
+    <Link to={`/note/${note._id}`}
+        className='card
+        bg-base-100
+        hover:shadow-lg
+        transition-all
+        duration-200
+        border-t-4
+        border-solid
+        border-[#00ff9d]'>
+
+        <div className='card-body'>
+            <h3 className='card-title text-base-content'>{note.title}</h3>
+            <p className='text-base-content/70 line-clamp-3'>{note.content}</p>
+            <div className='card-actions justify-between items-center mt-4'>
+                <span className='text-sm text-base-content/60'>
+                    {note.createdAt}
+                </span>
+
+                <div className='flex items-center gap-1'>
+                    <PenSquareIcon className='size-4'></PenSquareIcon>
+                    <Trash2Icon className='size-4'></Trash2Icon>
+                </div>
+            </div>
+
+        </div>
+
+    </Link>
   
   )
 }
